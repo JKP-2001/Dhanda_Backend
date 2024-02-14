3d80883c27bca98fa4ff424c671e179892e63626
unknown <llakshya63@gmail.com>
2024-02-14 17:23:28 +0530

    some changed

--- src/Controllers/Interview/InterviewerListController.js
+++ src/Controllers/Interview/InterviewerListController.js
@@ -1,14 +1,18 @@
 const { Instructor } = require("../../Models/peoples/Instructor");
-const { decryptFromJson } = require("../../Utils/EncryptDecrypt");
+const {
+    decryptFromJson,
+    encryptToJson,
+} = require("../../Utils/EncryptDecrypt");
 const { EncryptRes } = require("../../Utils/EncryptRes");
 const Paginator = require("../../helpers/Paginator");
-const { getNonSimilarityScore } = require("../../services/interviewerListControllerService");
+const {
+    getNonSimilarityScore,
+} = require("../../services/interviewerListControllerService");
 
-const BY_RATING = "rating";
+const BY_RATING = "Rating";
 const BY_NO_OF_INTERVIEWS_TAKEN = "interviewsTaken";
-const BY_PRICE_LOW_TO_HIGH = "priceLth";
-const BY_PRICE_HIGH_TO_LOW = "priceHtl";
-
+const BY_PRICE_LOW_TO_HIGH = "Price Low to High";
+const BY_PRICE_HIGH_TO_LOW = "Price High to Low";
 
 /**
  * @openapi
@@ -18,7 +22,7 @@ const BY_PRICE_HIGH_TO_LOW = "priceHtl";
  * /api/v1/interviewers/all:
  *   get:
  *     summary: Retrieve information based on payload.
- *     tags: 
+ *     tags:
  *       - Interviewers
  *     description: |
  *       Retrieves information based on the provided payload query parameter.
@@ -36,54 +40,52 @@ const BY_PRICE_HIGH_TO_LOW = "priceHtl";
  *         description: some
  */
 
-
 async function interviewerListController(req, res) {
     try {
         const queryParameters = decryptFromJson(req.query.fetchId);
         if (queryParameters == null) {
-            throw new Error("Query is not valid, ensure parameter fetchId with encrypted data is present")
+            throw new Error(
+                "Query is not valid, ensure parameter fetchId with encrypted data is present"
+            );
         }
 
         let sortBy = queryParameters.sortBy;
-        if (sortBy === undefined) sortBy = BY_RATING;
-        
+        if (sortBy === undefined || sortBy === ""||sortBy===null) sortBy = BY_RATING;
+
         const category =
             queryParameters.category === undefined ||
             queryParameters.category == "all"
                 ? {}
                 : { category: queryParameters.category };
-        
-        const companyNames = queryParameters.companies
-        console.log('company names ', companyNames)
+
+        const companyNames = queryParameters.companies;
+        console.log("company names ", companyNames);
 
         let instructors = await Instructor.find(
-            { ...category},
+            { ...category },
             "firstName lastName headline rating interviewsTaken price interviewDuration profilePic experience"
-        )
-        .populate('experience')
+        ).populate("experience");
 
-        console.log('instructors array is ', instructors)
-        let matchInstructors = instructors.filter(
-            (instructor,idx)=>{
-                if (companyNames.length == 0) return true
-                let instructorCompanyNames = []
-                instructor.experience.forEach((item,idx)=>{
-                    instructorCompanyNames.push(item.company)
-                })
-                const nomatch = getNonSimilarityScore(companyNames, instructorCompanyNames)
-                console.log("no match value is " , nomatch)
-                return nomatch == 0
-            }
-        )
-        matchInstructors = matchInstructors.map(
-            (instructor)=>{
-                delete instructor._doc.experience
-                return {...instructor._doc}
-            }
-        )
-        console.log('matchInstructor ', matchInstructors)
-        
-        
+        console.log("instructors array is ", instructors);
+        let matchInstructors = instructors.filter((instructor, idx) => {
+            if (companyNames.length == 0) return true;
+            let instructorCompanyNames = [];
+            instructor.experience.forEach((item, idx) => {
+                instructorCompanyNames.push(item.company);
+            });
+            const nomatch = getNonSimilarityScore(
+                companyNames,
+                instructorCompanyNames
+            );
+            console.log("no match value is ", nomatch);
+            return nomatch == 0;
+        });
+        matchInstructors = matchInstructors.map((instructor) => {
+            delete instructor._doc.experience;
+            return { ...instructor._doc };
+        });
+        console.log("matchInstructor ", matchInstructors);
+
         switch (sortBy) {
             case BY_RATING: {
                 matchInstructors = matchInstructors.sort(
@@ -120,12 +122,12 @@ async function interviewerListController(req, res) {
         // console.log('array ', matchInstructors)
         let results = Paginator(matchInstructors, page, limit);
 
-        if (process.env.DEVELOPMENT === 'false')
-            results = EncryptRes(results);
-        res.status(200).json({success:true, data:results});
+        if (process.env.DEVELOPMENT === "false")
+            results = encryptToJson(results);
+        res.status(200).json({ success: true, data: results });
     } catch (e) {
-        console.error('Error at interviewer list controller : ', e)
-        res.status(400).json({success:false, msg:e.toString()})
+        console.error("Error at interviewer list controller : ", e);
+        res.status(400).json({ success: false, msg: e.toString() });
     }
 }
 
