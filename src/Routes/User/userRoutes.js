fc3c95c700d1b8e776718bb3fefdcf502780952f
unknown <llakshya63@gmail.com>
2024-04-03 20:21:57 +0530

    contact form

--- src/Routes/User/userRoutes.js
+++ src/Routes/User/userRoutes.js
@@ -1,11 +1,12 @@
 
 const userRouter = require("express").Router();
 
-const { getUserData, onBoardingProcess, getUserDataById, handleTimeSlots } = require("../../Controllers/User/userControllers");
+const { getUserData, onBoardingProcess, getUserDataById, handleTimeSlots,contactus } = require("../../Controllers/User/userControllers");
 const { checkUser } = require("../../Middlewares/checkUser");
 
 userRouter.get("/user-data", checkUser, getUserData);
 userRouter.get("/user-data/:role/:id", getUserDataById);
+userRouter.post("/contactus", contactus);
 userRouter.patch("/onboarding", checkUser, onBoardingProcess);
 userRouter.patch("/handle-time-slots", checkUser, handleTimeSlots);
 
