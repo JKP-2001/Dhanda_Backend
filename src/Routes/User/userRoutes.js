762f60585362b145c07f443c538dfd86c189ee0c
Jitendra Kumar Pandey <84286157+JKP-2001@users.noreply.github.com>
2024-04-04 23:49:30 +0530

    Added Profile Pic API

--- src/Routes/User/userRoutes.js
+++ src/Routes/User/userRoutes.js
@@ -1,13 +1,30 @@
 
 const userRouter = require("express").Router();
 
-const { getUserData, onBoardingProcess, getUserDataById, handleTimeSlots,contactus } = require("../../Controllers/User/userControllers");
+const multer = require("multer");
+const { getUserData, onBoardingProcess, getUserDataById, handleTimeSlots,contactus, profileImageUpload } = require("../../Controllers/User/userControllers");
 const { checkUser } = require("../../Middlewares/checkUser");
+
+
+let upload;
+
+try{
+    upload = multer({
+        storage: multer.memoryStorage(),
+        limits: {
+          fileSize: 5 * 1024 * 1024, // limit file size to 5MB
+        },
+      });
+
+}catch(err){
+    console.log(err);
+}
 
 userRouter.get("/user-data", checkUser, getUserData);
 userRouter.get("/user-data/:role/:id", getUserDataById);
 userRouter.post("/contactus", contactus);
 userRouter.patch("/onboarding", checkUser, onBoardingProcess);
 userRouter.patch("/handle-time-slots", checkUser, handleTimeSlots);
+userRouter.patch("/profile-image-upload", checkUser, upload.array('files'), profileImageUpload);
 
 module.exports = userRouter
